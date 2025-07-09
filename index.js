96371cf7d72aca4dee0824eaa26fd60015771a43
Julie Rindone <julie.rindone@gmail.com>
2025-07-09 12:01:10 -0700

    Improved variable names

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ import { load } from 'https://cdn.jsdelivr.net/npm/cheerio@1.1.0/+esm'
 
 const wordCountInputEl = document.getElementById('word-count-input')
 const generateSmithsumBtnEl = document.getElementById('generate-smithsum-btm')
-const resultsWrapperEl = document.getElementById('results-wrapper')
+const lyricsBoxEl = document.getElementById('lyrics-box')
 const errorMessageEl = document.getElementById('error-message')
 const copyBtnEl = document.getElementById('copy-btn')
 let numWordsToGet = ''
-let songResult = ''
+let songToGet = ''
 
 generateSmithsumBtnEl.addEventListener('click', () => {
 	numWordsToGet = Number(wordCountInputEl.value.trim())
@@ -44,9 +44,8 @@ async function generateLoremSmithsum() {
 		// Do/While loop
 		//DO: get a random song Id. harvest its lyrics, and convert to songList array.
 		let indexToGet = Math.floor(Math.random(0, songList.length) * songList.length)
-		songResult = songList[indexToGet]
-		console.log(`songResult after await:`)
-		console.log(songResult)
+			songToGet = songList[indexToGet]
+			console.log(`songToGet after await: ` + songToGet)
 
 
 		// WHILE: the actual wordcount is <  numWordsToGet
@@ -57,17 +56,17 @@ async function generateLoremSmithsum() {
 				// If numwordstoget < array.length, grab that number of words and display. if it's greater, call  getSongList again. 
 				// PROBLEM: is this going to fuck up the async stuff? I think some of this definitely needs to be moved out of the generateLoremSmithsum() function.
 
-		// TODO: Decide on results formatting - how do I want the lyrics to look? remove breaks, possibly punctuation.
+			// TODO: Decide on lyrics formatting - how do I want the lyrics to look? remove breaks, possibly punctuation.
 
-		// TODO: Convert string to 
+			// TODO: Convert string to array
 
-		// Display results box
-		resultsWrapperEl.classList.replace('empty-results-wrapper', 'results-wrapper')
-		copyBtnEl.classList.replace('empty-results-wrapper', 'copy-btn')
+			// Display lyricsWrapper box
+			lyricsBoxEl.classList.replace('empty-lyrics-box', 'lyrics-box')
+			copyBtnEl.classList.replace('empty-lyrics-box', 'copy-btn')
 
 		// TEMPORARY: DISPLAY ID # for songList[0]
-		// Insert results
-		displayLyrics(songResult) // This stays synchronous! (Within the order of the function, I guess.)
+			// Insert lyrics
+			displayLyrics(songToGet) // This stays synchronous! (Within the order of the function, I guess.)
 	}
 	catch (err) {
 		console.log(err)
@@ -84,7 +83,7 @@ function getCorrectwordCountInputEl() {
 	////////////// TODO: ** IS THIS PROBLEM STILL HAPPENING? CANT REMEMBER. Check it out.
 
 	// Clear any existing content
-	resultsWrapperEl.innerHTML = ''
+	lyricsBoxEl.innerHTML = ''
 	// numWordsToGet = ''
 	// console.log("Getting # words: " + numWordsToGet);
 
@@ -94,26 +93,27 @@ function getCorrectwordCountInputEl() {
 function clearerrorMessageEl() {
 	errorMessageEl.innerHTML = ''
 	wordCountInputEl.value = ''
-	resultsWrapperEl.textContent = ''
+	lyricsBoxEl.textContent = ''
 }
 
-function displayLyrics(songResult) {
+function displayLyrics(songToGet) {
 	// console.log(`word count is ${numWordsToGet}`);
 	console.log(`displayLyrics() function called.`);
-	console.log("songResult is " + songResult)
+	console.log("songToGet is " + songToGet)
+	console.log(`word count is ${numWordsToGet}`);
 
 	const lyricsEl = document.createElement("p")
 	lyricsEl.id = 'lyrics-el'
-	let lyrics = "girlfriend in a coma i know, i know, it's serious. there were times when I could've murdered her."
+	let testLyrics = "girlfriend in a coma i know, i know, it's serious. there were times when I could've murdered her."
 
-	// passing songResult is temporary! It'll only be used to get that song's yrics
-	lyricsEl.innerHTML = songResult + ":&ensp;" + lyrics
+	// passing songToGet is temporary! It'll only be used to get that song's yrics
+	lyricsEl.innerHTML = songToGet + ":&ensp;" + testLyrics
 
 
 	// I need to have the previously generated lyrics cleared out, which means removing the appended paragraph before running this line again.
-	document.getElementById('lyrics-el') ? "there is at least one lyrics element." : "no lyrics element exists."
-	// resultsWrapperEl.removeChild(lyricsEl)
-	resultsWrapperEl.appendChild(lyricsEl)
+	document.getElementById('lyrics-el') ? "there is at least one testLyrics element." : "no testLyrics element exists."
+	// lyricsBoxEl.removeChild(lyricsEl)
+	lyricsBoxEl.appendChild(lyricsEl)
 }
 
 // It doesn't seem to matter if this is async or not.
@@ -126,31 +126,25 @@ async function copyLyricsToClipboard() {
 }
 
 async function getSongList() {
-	const smithsSongs = 'https://songmeanings.com/artist/view/songs/464/'
+	console.log(`inside song:istURL()`);
+
+	const songListUrl = 'https://songmeanings.com/artist/view/songs/464/'
 	//  get data from axios
-	const { data } = await axios.get(smithsSongs)
+	const { data } = await axios.get(songListUrl)
 	// pass data to cheerio
-	const $ = load(data)
-	// declare results variable
-	const results = []
+	let $ = load(data)
+	// declare arrayOfSongIds variable
+	const arrayOfSongIds = []
 	// Loop through needed data (songs) & push to array
 	$('#songslist tr').each((i, elem) => {
 		// get ID of each song
 		const songId = $(elem).attr('id');
 
-		// add song-id to results array
-		results.push(songId);
+		// add song-id to arrayOfSongIds array
+		arrayOfSongIds.push(songId);
 	})
-	return results;
+	return arrayOfSongIds;
 }
-
-
-
-
-
-
-
-// let lyrics = "girlfriend in a coma i know, i know, it's serious. there were times when I could've murdered her.But I'd hate anything to happen to her. No, I don't want to see her.Do you really think she'll pull through? Do you really think she'll pull through ? Let me whisper my last goodbye, I know it's serious. girlfriend in a coma i know, i know, it's serious.there were times when I could've murdered her. But I'd hate anything to happen to her.No, I don't want to see her. Do you really think she'll pull through ? Do you really think she'll pull through? Let me whisper my last goodbye, I know it's serious.girlfriend in a coma i know, i know, it's serious. there were times when I could've murdered her.But I'd hate anything to happen to her."
 
 
 // how i was using getSongList() before converting generateLorumSmithsum() into async:
